8596b49d980a5dfadf46b15b29a36a63fa118798
Carlos Gandolfo W <carlos.gandolfo@externos.bci.cl>
2021-07-29 12:04:41 -0400

    Cambio 7

--- src/test/java/pages/RetomarContratacionPage.java
+++ src/test/java/pages/RetomarContratacionPage.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.WebDriver;
 import utilidades.ClaseBase;
 
 public class RetomarContratacionPage extends ClaseBase {
-
+//Cambio 7
     public RetomarContratacionPage(WebDriver driver) {
         super(driver);
     }
-
+	//Cambio 7
     //Identificar localizadores
     By locatorBtnRetomarContratacion =By.xpath("//button[contains(text(),'contrat')]");
 
